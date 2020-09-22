6f6da55a8362fbef7ec7f01051e4ec230744a9ff
zldell <zldell@git>
2020-09-22 19:45:26 +0200

    highway flag

--- question_classification/models/hier_qc.py
+++ question_classification/models/hier_qc.py
@@ -6,19 +6,21 @@ from question_classification.models.highway import Highway
 
 
 class HierRNN(nn.Module):
-    def __init__(self, num_embeddings, embedding_dim, low_hidden_size, high_hidden_size, num_classes, max_character_len, max_sentence_len):
+    def __init__(self, num_embeddings, embedding_dim, low_hidden_size, high_hidden_size, num_classes, max_character_len, max_sentence_len, use_highway=False):
         # # TODO: Workout the dimensions.
         # TODO: Make it work with att lstm
         super(HierRNN, self).__init__()
         # The five layers of the hierchical lstm:
         self.character_embedding = nn.Embedding(num_embeddings, embedding_dim)
-        self.highway = Highway(low_hidden_size * max_character_len, low_hidden_size * max_character_len)
-        # self.low_attlstm = something
-        # self.high_attlstm = something
+
+        if use_highway:
+            self.highway = Highway(low_hidden_size * max_character_len, low_hidden_size * max_character_len)
         self.low_lstm = nn.LSTM(input_size=embedding_dim, hidden_size=low_hidden_size, batch_first=True)
         self.high_lstm = nn.LSTM(input_size=low_hidden_size * max_character_len, hidden_size=high_hidden_size, batch_first=True)
         self.output_layer = nn.Linear(high_hidden_size * max_sentence_len, num_classes)
         self.low_hidden_size = low_hidden_size
+
+        self.use_highway = use_highway
 
     def low_reset_states(self, batch_size):
         return torch.zeros(batch_size, self.low_hidden_size), torch.zeros(batch_size, self.low_hidden_size)
@@ -34,8 +36,9 @@ class HierRNN(nn.Module):
             embedded_word = self.character_embedding(x[:, i, :])
             hidden = self.low_reset_states(batch_size)
             low_out, hidden = self.low_lstm(embedded_word)
-            low_out = low_out.contiguous().view((batch_size, - 1))
-            word_repr = self.highway(low_out)
+            word_repr = low_out.contiguous().view((batch_size, - 1))
+            if self.use_highway:
+                word_repr = self.highway(word_repr)
             sentence.append(word_repr)
         sentence = torch.stack(sentence)
         sentence_repr, hidden = self.high_lstm(sentence)
